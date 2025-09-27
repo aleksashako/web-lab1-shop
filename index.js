f04c610c332f9802907236cac37a5b08b56dd05a
aleksandra <sashka_kolm@mail.ru>
2025-09-28 01:06:07 +0300

    images fixed

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const things = [{
     },
     {
     id: 3,
-    image: "img/clock-without-arrows.jpg",
+    image: "../../img/clock-without-arrows.jpg",
     title: "Clock (Without Arrows)",
     description: "no comments.",
     price: 19,  
@@ -29,9 +29,9 @@ const things = [{
     {
     id: 5,
     image: "",
-    title: "Book Without Cover",
-    description: "still a good book though",
-    price: 6,  
+    title: "...",
+    description: "...",
+    price: 0,  
     }
 ];
 
